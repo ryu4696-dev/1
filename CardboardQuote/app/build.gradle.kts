d0cb7be246070e208bbf72d2acd80e14e419781f
ryu4696-dev <ryu4696@gmail.com>
2026-09-25 23:23:15 +0900

    Bump cardboard quote app to v1.6

--- CardboardQuote/app/build.gradle.kts
+++ CardboardQuote/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
     namespace = "jp.co.kobayashi.cardboardquote"
     compileSdk = 34
-    defaultConfig { applicationId = "jp.co.kobayashi.cardboardquote"; minSdk = 26; targetSdk = 34; versionCode = 5; versionName = "1.5.0" }
+    defaultConfig { applicationId = "jp.co.kobayashi.cardboardquote"; minSdk = 26; targetSdk = 34; versionCode = 6; versionName = "1.6.0" }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
     kotlinOptions { jvmTarget = "17" }
 }

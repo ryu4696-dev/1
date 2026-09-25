ff284fc418c253ada6656d3f62238e30238fe96c
ryu4696-dev <ryu4696@gmail.com>
2026-09-25 21:38:29 +0900

    Add CardboardQuote/app/build.gradle.kts

---
+++ CardboardQuote/app/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android {
+    namespace = "jp.co.kobayashi.cardboardquote"
+    compileSdk = 34
+    defaultConfig { applicationId = "jp.co.kobayashi.cardboardquote"; minSdk = 26; targetSdk = 34; versionCode = 1; versionName = "1.0.0" }
+    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
+    kotlinOptions { jvmTarget = "17" }
+}
